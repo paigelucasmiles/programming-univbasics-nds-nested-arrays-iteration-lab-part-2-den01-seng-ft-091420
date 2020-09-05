e99359f9d4a0e8cbc3f53bf42a4981215027383f
Paige Miles <paige@lucas-miles.com>
2020-09-05 19:26:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,7 @@
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
+
+
 def find_min_in_nested_arrays(array_of_daily_temperatures)
   row_index = 0
   daily_min_temp = []
